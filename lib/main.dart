6998db374007dff244882348c60abd65a4e589d9
jcerveram <cerveramonzo@gmail.com>
2023-09-21 20:24:28 +0200

    img centrada

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,20 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(" La Barra de arriba"),
-          centerTitle: true,
-        ),
-        body: Container(
+      appBar: AppBar(
+        title: const Text(" La Barra de arriba"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
           width: 300, // Ancho deseado de la imagen
           height: 300, // Alto deseado de la imagen
           child: Image.network(
-              "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen-800x419.jpg"),
-        ));
+            "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen-800x419.jpg",
+            fit: BoxFit.cover, // Ajusta la imagen para cubrir el contenedor
+          ),
+        ),
+      ),
+    );
   }
 }
