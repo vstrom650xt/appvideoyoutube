308dd64df614a522c51999b835132711d7baf6ac
jcerveram <cerveramonzo@gmail.com>
2023-09-22 19:10:41 +0200

    dato centrado y otro no

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Miapp());
+void main() => runApp(const Miapp());
 
 class Miapp extends StatelessWidget {
   const Miapp({super.key});
@@ -25,34 +25,20 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" La Barra de arriba"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(
-                20), // hace la img mas pequeña en funcion de la pantalla en q se vea
-            // width: 200, // Ancho deseado de la imagen
-            //   height: 200, // Alto deseado de la imagen
-            child: Image.network(
-              "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen-800x419.jpg",
-              //  fit: BoxFit.cover, // Ajusta la imagen para cubrir el contenedor
+        appBar: AppBar(
+          title: const Text(" La Barra de arriba"),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Text("data"),
             ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              child: ElevatedButton(
-                  onPressed: () {
-                    print("hola");
-                  },
-                  child: Text('Boton')),
-            ),
-          )
-        ],
-      ),
-    );
+            Text("data")
+          ],
+        ));
   }
 }
